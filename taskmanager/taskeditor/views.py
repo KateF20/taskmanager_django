e443a16930cc765f9cb11702cf8b06aedadbf1f3
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-04-21 17:25:25 +0200

    delete task from tasks list

--- taskmanager/taskeditor/views.py
+++ taskmanager/taskeditor/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Task
-from .forms import TaskForm
+from .forms import TaskForm, NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 def index(request):
-    tasks = Task.objects.order_by('-id')
-    return render(request, 'main/index.html', {'title': 'Main page', 'tasks': tasks})
+    if request.user.is_authenticated:
+        tasks = Task.objects.filter(user=request.user).order_by('-id')
+        return render(request, 'main/index.html', {'title': 'Main page', 'tasks': tasks})
+    else:
+        return render(request, 'main/index.html')
 
 
 def about(request):
@@ -30,5 +35,16 @@ def create(request):
     return render(request, 'main/create.html', context)
 
 
-def home(request):
-    return render(request, 'registration/home.html')
+def registration(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')
+            return redirect('/')
+    else:
+        form = NewUserForm()
+
+    context = {'form': form}
+    return render(request, 'registration/registration.html', context)
